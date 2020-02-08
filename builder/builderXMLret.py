3e0ad832501dc86ddbcc0cec1e2b78bf7d2490d7
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-02-08 19:59:19 -0300

    AUT-01 Refactoring do código

--- builder/builderXMLret.py
+++ builder/builderXMLret.py
@@ -1,56 +1,45 @@
 import re
 
 
-def extractor(teste):
-    teste = str(teste)
-    texto = teste.split("<TABLE")
-    for i in texto:
+def extractor(test):
+    test_str = str(test)
+    text = test_str.split("<TABLE")
+    for i in text:
         if re.search('<span id="lblRetorno">', i):
-            recorte = i.replace('\t\t\t\t\t\t\t\t<span id="lblRetorno">', '')
-            recorte = recorte.replace('</span>', '')
-            recorte = recorte.replace('border="0" width="90%" align="center">', '')
-            recorte = recorte.replace('\r\n\t\t\t\t\t<TR>', '')
-            recorte = recorte.replace('\r\n\t\t\t\t\t\t<TD>', '')
-            recorte = recorte.strip()
-            initial_position = recorte.find('<P align="justify">')
-            final_position = recorte.find('</P>')
-            recorte = recorte[initial_position:final_position + 4]
+            cut = i.replace('\t\t\t\t\t\t\t\t<span id="lblRetorno">', '')
+            cut = cut.replace('</span>', '')
+            cut = cut.replace('border="0" width="90%" align="center">', '')
+            cut = cut.replace('\r\n\t\t\t\t\t<TR>', '')
+            cut = cut.replace('\r\n\t\t\t\t\t\t<TD>', '')
+            cut = cut.strip()
+            initial_position = cut.find('<P align="justify">')
+            final_position = cut.find('</P>')
+            cut = cut[initial_position:final_position + 4]
             break
-    return recorte
+    return cut
 
-def getSchema(identificacao, bloco, nome_campo1 , nome_campo2 , nome_campo3 , campo1, campo2, campo3):
-    sb = []
-    sb.append('<?xml version="1.0" encoding="ISO-8859-1"?>\n')
-    sb.append('<Site>\n')
-    sb.append('\t<Identificacao>')
-    sb.append(identificacao)
-    sb.append('</Identificacao>\n')
-    sb.append('\t<Corpo>\n')
-    sb.append('\t\t<L nome="' + bloco + '">\n')
-    sb.append('\t\t\t<B>\n')
-    sb.append('\t\t\t\t<C nome ="' + nome_campo1 + '">')
-    sb.append(campo1)
-    sb.append('</C>\n')
-    sb.append('\t\t\t\t<C nome ="' + nome_campo2 + '">')
-    sb.append(campo2)
-    sb.append('</C>\n')
-    sb.append('\t\t\t\t<C nome ="' + nome_campo3 + '">')
-    sb.append(campo3)
-    sb.append('</C>\n')
-    sb.append('\t\t\t</B>\n')
-    sb.append('\t\t</L>\n')
-    sb.append('\t</Corpo>\n')
-    sb.append('</Site>')
+
+def get_schema(identificacao, bloco, nome_campo1, nome_campo2, nome_campo3, campo1, campo2, campo3):
+    sb = ['<?xml version="1.0" encoding="ISO-8859-1"?>\n', '<Site>\n', '\t<Identificacao>', identificacao,
+          '</Identificacao>\n', '\t<Corpo>\n', '\t\t<L nome="' + bloco + '">\n', '\t\t\t<B>\n',
+          '\t\t\t\t<C nome ="' + nome_campo1 + '">', campo1, '</C>\n', '\t\t\t\t<C nome ="' + nome_campo2 + '">',
+          campo2, '</C>\n', '\t\t\t\t<C nome ="' + nome_campo3 + '">', campo3, '</C>\n', '\t\t\t</B>\n', '\t\t</L>\n',
+          '\t</Corpo>\n', '</Site>']
 
     return ''.join(sb)
 
-class BuilderXMLret:
 
-    def builder_xml_ret(html):
+class BuilderXmlRet:
+
+    def __init__(self, html):
+        self.html = html
+        self.builder_xml_ret(html)
+
+    def builder_xml_ret(self, html):
         html = html
-        infor_registro = ''
-        dados_liberacao = ''
-        mensagem = ''
+        info_reg = ''
+        data_liberation = ''
+        message = ''
         recorded = extractor(html)
         initial_position = recorded.find('<P align="justify">&nbsp;')
         final_position = recorded.find('</P>')
@@ -63,19 +52,18 @@ class BuilderXMLret:
 
             for i in array:
                 if re.search('Veículo placa', i):
-                    infor_registro = i
+                    info_reg = i
                 elif re.search('A liberação DO veículo', i) or re.search('A liberação DO veículo', i) or re.search(
                         'De segunda a sexta-feira', i) or re.search('    DETRAN', i) or re.search('2\)',
                                                                                                   i) or re.search(
-                        'O proprietário deverá', i):
-                    dados_liberacao += i
+                    'O proprietário deverá', i):
+                    data_liberation += i
                 elif re.search('O veículo será liberado', i):
-                    mensagem = i
+                    message = i
 
-            xml = getSchema('GUINCHADOSPREFEITURASP', 'Registro', 'inforRegistro', 'dadosLiberacao', 'mensagem',
-                            infor_registro, dados_liberacao, mensagem)
+            xml = get_schema('GUINCHADOSPREFEITURASP', 'Registro', 'inforRegistro', 'dadosLiberacao', 'message',
+                             info_reg, data_liberation, message)
             print(xml)
-
 
     # html = open('../texto.html', 'r')
     # teste = html.readlines()
